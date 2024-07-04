f07f379730b6eb9b113b1d03682a6bd9c6768a2c
smartajay2 <ajayjest2@gmail.com>
2024-07-05 04:28:53 +0530

    Normalize line endings according to .gitattributes

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+export default function Header(){
+  return(
+  <div className="p-2 bg-black">
+         <div className="p-12 text-black bg-white text-2xl">
+            <h1>Hey...</h1>
+            <h1>I'am Ajay..</h1>
+            <h1>I develop Full stack Wep Applications</h1>
+         </div>
+         <div>
+             <img src="/"/>
+         </div>
+         <div>
+             <p className="text-black bg-white pl-10 pr-10 text-2xl">Hi, I'm Ajay Jestin. I have completed my B.Sc. in Electronics and Communication, and I am passionate about web development. I have successfully developed an e-commerce website and a bulk mail application using modern web technologies.</p>
+         </div>
+  </div>
+  )
+}
