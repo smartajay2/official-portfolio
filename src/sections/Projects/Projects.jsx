68a105fc038d35b5afdfb7a5641cb0935a6a1c91
smartajay2 <ajayjest2@gmail.com>
2024-08-25 22:30:49 +0400

    newly profile

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ProjectCard from '../../common/ProjectCard';
-import Viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png'
-import hipsster from '../../assets/hipsster.png'
-import Fitlift from '../../assets/fitlift.png'
+import Weather from '../../assets/weather.png';
+import Blog from '../../assets/blog.png'
+import bulk from '../../assets/bulk mail.png'
+import Fitlift from '../../assets/.png'
 
 
 
@@ -14,22 +14,22 @@ function Projects() {
     <h1 className="text-3xl font-bold mb-6">Projects</h1>
     <div className="flex flex-wrap justify-center lg:justify-between w-full gap-6 lg:flex-row">
       <ProjectCard
-        src={Viberr}
+        src={Weather}
         link={'https://www.google.com/search?q=apple+store'}
         h3="Viberr"
-        p="Streaming App"
+        p="Weather App"
       />
       <ProjectCard
-        src={freshBurger}
+        src={Blog}
         link={'https://www.google.com/search?q=apple+store'}
         h3="Fresh Burger"
-        p="Number Restaurant"
+        p="Blog App"
       />
       <ProjectCard
-        src={hipsster}
+        src={bulk}
         link={'https://www.google.com/search?q=apple+store'}
-        h3="Hipsster"
-        p="Glasses Shop"
+        h3="Bulk Mail"
+        p="Bulk Mail App"
       />
       <ProjectCard
         src={Fitlift}
